e0710f79f1d733753761ed4d3d179402ecb4820b
AbbyMoraa <abbymoraa876@gmail.com>
2026-02-09 04:55:15 -0500

    Products (#8)
    * products cards designed
    
    * edited landing page
    
    * edited navbar and product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,59 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Register from "./pages/Register.jsx";
-import Login from "./pages/Login.jsx";
-import Profile from "./pages/Profile.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const App = () => {
+import Landing from "./pages/Landing";
+import Home from "./pages/Home.jsx";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile.jsx";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+
+        <main className="flex-grow">
+          <Routes>
+            {/* Public pages */}
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Protected pages */}
+            <Route
+              path="/cart"
+              element={
+                <ProtectedRoute>
+                  <Cart />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "../features/products/productSlice";
 import authReducer from "../features/auth/authSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
+    products: productReducer,
     auth: authReducer,
   },
 });
 
-export default store;
 
